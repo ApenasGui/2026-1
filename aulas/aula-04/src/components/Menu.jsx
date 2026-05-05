761ba53d6a07856c3f316af98e8ae1117b6058e9
ApenasGui <guilherme.s.costa1@gmail.com>
2026-05-05 09:26:48 -0300

    refatora o Menus.jsx para comportar página padrão de usuário

--- aulas/aula-04/src/components/Menu.jsx
+++ aulas/aula-04/src/components/Menu.jsx
@@ -2,10 +2,12 @@ import { Link, NavLink } from "react-router"
 import "./Menu.css";
 
 export default function Menu() {
+    const userId = 0;
+
     return <nav>
         <ul>
             <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/perfil">Perfil</NavLink></li>
+            <li><NavLink to={`/perfil/${userId}`}>Perfil</NavLink></li>
             <li><NavLink to="/settings">Configurações</NavLink></li>
             <li><NavLink to="/about">Sobre</NavLink></li>
             <li><Link to="/login">Login</Link></li>
